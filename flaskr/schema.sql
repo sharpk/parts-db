dbff27c51dfa4c69e41c68fc26ee0c30f042009b
Ken Sharp <ksharp.pub@gmail.com>
2020-02-07 16:12:27 -0600

    Change default db schema
    Define the new part table and remove the old post table.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -2,7 +2,7 @@
 -- Drop any existing data and create empty tables.
 
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+-- DROP TABLE IF EXISTS part;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,11 +10,29 @@ CREATE TABLE user (
   password TEXT NOT NULL
 );
 
-CREATE TABLE post (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id INTEGER NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES user (id)
-);
+-- a new parts table is created for each user, the schema looks like this
+-- CREATE TABLE part (
+--  `id` int(6) NOT NULL DEFAULT '0',
+--  `type` char(50) NOT NULL,
+--  `class` int(8) NOT NULL DEFAULT '-1',
+--  `qty` int(5) NOT NULL,
+--  `pkg` char(7) NOT NULL,
+--  `manuf` char(25) DEFAULT NULL,
+--  `partnum` char(25) DEFAULT NULL,
+--  `cost` char(25) DEFAULT NULL,
+--  `location` char(25) NOT NULL,
+--  `description` varchar(255) NOT NULL,
+--  `notes` varchar(255) DEFAULT NULL,
+--  `url` varchar(255) NOT NULL,
+--  PRIMARY KEY (`id`)
+--);
+
+-- old blog post schema from flaskr example
+--CREATE TABLE post (
+--  id INTEGER PRIMARY KEY AUTOINCREMENT,
+--  author_id INTEGER NOT NULL,
+--  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--  title TEXT NOT NULL,
+--  body TEXT NOT NULL,
+--  FOREIGN KEY (author_id) REFERENCES user (id)
+--);
