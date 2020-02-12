350ace9722b490f27414a6de1e3836e384358ed3
Ken Sharp <ksharp.pub@gmail.com>
2020-02-12 12:13:03 -0600

    Add Browse page
    Re-work the previous blog post list to query and list all parts in a big table.
    
    The table is ugly but at least displays data successfully.

--- flaskr/parts.py
+++ flaskr/parts.py
@@ -19,16 +19,15 @@ def index():
 	return render_template("parts/index.html")
 
 @bp.route("/browse")
+@login_required
 def browse():
     """Show all the posts, most recent first."""
     db = get_db()
-    posts = db.execute(
-        "SELECT p.id, title, body, created, author_id, username"
-        " FROM post p JOIN user u ON p.author_id = u.id"
-        " ORDER BY created DESC"
+    tablename = "parts"+g.user['username']
+    parts = db.execute(
+        "SELECT type, class, qty, pkg, manuf, partnum, cost, location, description, notes, url FROM " + tablename,
     ).fetchall()
-    return render_template("blog/index.html", posts=posts)
-
+    return render_template("parts/browse.html", parts=parts)
 
 def get_post(id, check_author=True):
     """Get a post and its author by id.
