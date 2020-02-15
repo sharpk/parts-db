2bddc64b173926414fe923256dfb47c603017d11
Ken Sharp <ksharp.pub@gmail.com>
2020-02-14 22:22:12 -0600

    Add Class XML parser

---
+++ flaskr/classxml.py
@@ -0,0 +1,38 @@
+import xml.etree.ElementTree as ET
+
+root = None
+
+def parse_hierarchy_xml(xml_file):
+	global root
+	tree = ET.parse(xml_file)
+	root = tree.getroot()
+
+def find_child_by_id(id):
+	for child in root.iter('child'):
+		if child.attrib['id'] == id:
+			return child
+	return None
+
+# get string of class names starting from root to given id
+def get_class_string(id):
+	delimiter = '.'
+	name_list = list()
+	class_iter = find_child_by_id(id)
+	while class_iter != None and class_iter.tag == 'child':
+		name_list.append(class_iter.attrib['name'])
+		class_iter = root.find('.//child[@id="' + class_iter.attrib['id'] + '"]/..')  # get parent node
+	name_list.reverse()
+	return_str = ""
+	for name in name_list:
+	  return_str = return_str + delimiter + name
+	return return_str[1:]
+
+# get list of child id values for given id
+def get_class_children(id):
+	id_list = list()
+	
+	start_class = find_child_by_id(id)
+	if start_class:
+		for child in start_class.iter('child'):
+			id_list.append(child.attrib['id'])
+	return id_list
